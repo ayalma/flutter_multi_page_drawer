51a4869764130933d6de1d30d16689be6597f5ea
ali <alimohammadi7117@gmail.com>
2018-12-25 12:29:29 +0330

    add support for navigation and closing drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: '/',
-      routes: {
-        // When we navigate to the "/" route, build the FirstScreen Widget
-        '/': (context) => FirstScreen(),
-        // When we navigate to the "/second" route, build the SecondScreen Widget
-        '/second': (context) => SecondScreen(),
-      },
-      builder: (context, child) {
-        return Scaffold(
-          drawer: MyDrawer(),
-          body: child,
-        );
-      },
-    );
-  }
-}
-
+void main() => runApp(MultipageDrawerApp());
 
 class MultipageDrawerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter multi page drawer',
       initialRoute: '/',
       routes: {
         // When we navigate to the "/" route, build the FirstScreen Widget
@@ -40,7 +16,7 @@ class MultipageDrawerApp extends StatelessWidget {
       },
       builder: (context, child) {
         return Scaffold(
-          drawer: MyDrawer(),
+          drawer: MyDrawer(key:Key("main_drawer"),navigator: (child.key as GlobalKey<NavigatorState>)),
           body: child,
         );
       },
@@ -49,25 +25,97 @@ class MultipageDrawerApp extends StatelessWidget {
 }
 
 class MyDrawer extends StatelessWidget {
+  final GlobalKey<NavigatorState> navigator;
+
+  const MyDrawer({Key key, this.navigator}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Text("this is drawer"),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32.0),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 120.0,
+                      height: 120.0,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white),
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://avatars3.githubusercontent.com/u/15701316?s=460&v=4"))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Ali Mohammadi",
+                          style: TextStyle(fontSize: 22, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.dashboard),
+            title: Text('First page'),
+            onTap: () {
+              navigator.currentState.pushNamed("/");
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.send),
+            title: Text('Second page'),
+            onTap: () {
+
+              navigator.currentState.pushNamed("/second");
+
+              var x = RootDrawer.of(context);
+              x.close();
+              print(x);
+
+             // RootScaffold.openDrawer(context);
+              //RootScaffold.of(context)
+              //navigator.currentState.pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
 
-
 class RootScaffold {
   static openDrawer(BuildContext context) {
     final ScaffoldState scaffoldState =
-    context.rootAncestorStateOfType(TypeMatcher<ScaffoldState>());
+        context.rootAncestorStateOfType(TypeMatcher<ScaffoldState>());
     scaffoldState.openDrawer();
+  }
+
+  static ScaffoldState of(BuildContext context) {
+    final ScaffoldState scaffoldState =
+    context.rootAncestorStateOfType(TypeMatcher<ScaffoldState>());
+    return scaffoldState;
+
   }
 }
 
-
 class FirstScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,7 +123,7 @@ class FirstScreen extends StatelessWidget {
         title: Text("First screen"),
         leading: IconButton(
           icon: Icon(Icons.menu),
-          onPressed: (){
+          onPressed: () {
             RootScaffold.openDrawer(context);
           },
         ),
@@ -108,5 +156,15 @@ class SecondScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+class RootDrawer
+{
+  static DrawerControllerState of(BuildContext context){
+    final DrawerControllerState drawerControllerState =
+    context.rootAncestorStateOfType(TypeMatcher<DrawerControllerState>());
+    return drawerControllerState;
   }
 }
